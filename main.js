47240327ee0a48717a53cd1e7adec5411be6406d
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-10 19:21:53 +0100

    added click functions (toggle color and img)

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ var app = new Vue({
         el: "#root",
         data: {
             message: "Hello Word! Welcome Vue!",
-            source: "https://images.everyeye.it/img-notizie/ecco-linguaggi-programmazione-prova-futuro-v7-531835-900x900.webp"
+            source: "https://unsplash.it/300/300?image=1",
+            color: "red"
+        },
+        methods: {
+            changeImg: function() {
+                if (this.source === "https://unsplash.it/300/300?image=1"){
+                    this.source = "https://unsplash.it/300/300?image=2"
+                } else {
+                    this.source = "https://unsplash.it/300/300?image=1"
+                }
+            },
+            changeColor: function() {
+                if (this.color === "red"){
+                    this.color = "blue"
+                } else {
+                    this.color = "red"
+                }
+            }
         }
-})
+    })
