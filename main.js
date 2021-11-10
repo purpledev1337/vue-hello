5e04847bb870083951397d009379d3924bfbc9cb
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-10 16:08:31 +0100

    DONE h1 content + image printing

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+        el: "#root",
+        data: {
+            message: "Hello Word! Welcome Vue!",
+            source: "https://images.everyeye.it/img-notizie/ecco-linguaggi-programmazione-prova-futuro-v7-531835-900x900.webp"
+        }
+})
